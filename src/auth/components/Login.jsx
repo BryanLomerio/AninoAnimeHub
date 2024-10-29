d3bbe11512dba686616cd6efb483b89ac75d70b7
anino secret <bryanlomerioanino@gmail.com>
2024-10-30 06:04:00 +0800

    test

--- src/auth/components/Login.jsx
+++ src/auth/components/Login.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import { FaGoogle, FaFacebook, FaTwitter } from 'react-icons/fa';
 import './Login.css';
-import backgroundImage from "/src/assets/thumb-1920-1378545.jpg";// Adjust the path accordingly
 
 function Login() {
     return (
-        <div className='login-container' style={{
-            backgroundImage: `url(${backgroundImage})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            zIndex: 999,
-        }}>
+        <div className='login-container'>
             <div className='login-form'>
                 <h1>Login</h1>
                 <input type="text" placeholder="Email" className='input-field' required />
                 <input type="password" placeholder="Password" className='input-field' required />
                 <div className='forgot-password'>
-                    <input type="checkbox" /> Remember me
+                    <input type="checkbox" label='checkbox' /> Remember me
                     <p>Forgot Password</p>
                 </div>
                 <div className='button-container'>
